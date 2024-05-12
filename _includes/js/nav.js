219197d2c838d24a858b9408c72a2e9566619653
Michał Pstrąg <michal.pstrag@icloud.com>
2024-05-12 22:27:17 +0200

    redesign navbar

--- _includes/js/nav.js
+++ _includes/js/nav.js
@@ -5,7 +5,7 @@
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 18) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
