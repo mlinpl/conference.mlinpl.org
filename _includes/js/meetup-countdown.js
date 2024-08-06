78d9511c3c0f4fee4573658b48f3b0c44f413393
Michał Pstrąg <michal.pstrag@icloud.com>
2024-08-07 01:02:54 +0200

    add meetups page

---
+++ _includes/js/meetup-countdown.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/*!
+ * ML in PL Jekyll Theme - Countdown
+ * This script initiates countdown to the next meetup
+ */
+
+var meetupCountdownElement = document.getElementById("meetup-countdown");
+var last_meetup_header = "/ Last meetup"
+var next_meetup_header = "/ Next meetup"
+
+if (meetupCountdownElement) {
+    countdown(
+        function(ts) {
+            console.log(ts);
+            if (ts.value < 0) {
+                meetupCountdownElement.innerHTML = last_meetup_header;
+            }
+            else if (ts.days == 0) {
+                meetupCountdownElement.innerHTML = next_meetup_header + " today";
+            }
+            else if (ts.days == 1) {
+                meetupCountdownElement.innerHTML = next_meetup_header + " in " + ts.days + " day";
+            }
+            else if (ts.days > 1) {
+                meetupCountdownElement.innerHTML = next_meetup_header + " in " + ts.days + " days";
+            }
+        },
+        new Date("{{ site.header.meetup-countdown-date }}"),
+        countdown.DAYS,
+    );
+}
