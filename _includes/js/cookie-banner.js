d8e5e9ea2cc0f78273b76d3634b303e80e2f78a5
arekpaterak <arkadiusz.paterak@gmail.com>
2025-06-11 16:31:23 +0200

    Load Google Analytics

--- _includes/js/cookie-banner.js
+++ _includes/js/cookie-banner.js
@@ -1,18 +1,35 @@
-// Show banner if no choice has been made
 (function() {
   var banner = document.getElementById('cookie-banner');
-  var accepted = localStorage.getItem('ga_cookie_consent');
-  if (!accepted) {
+  var consent = localStorage.getItem('ga_cookie_consent');
+
+  if (!consent) {
     banner.style.display = 'block';
+  } else if (consent === 'accepted') {
+    loadGoogleAnalytics();
   }
 
   document.getElementById('cookie-accept').onclick = function() {
     localStorage.setItem('ga_cookie_consent', 'accepted');
     banner.style.display = 'none';
+    loadGoogleAnalytics();
   };
 
   document.getElementById('cookie-deny').onclick = function() {
     localStorage.setItem('ga_cookie_consent', 'denied');
     banner.style.display = 'none';
   };
+
+  function loadGoogleAnalytics() {
+    var script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-16939342125';
+    script.async = true;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+
+    gtag('js', new Date());
+    gtag('config', 'AW-16939342125');
+  }
 })();
